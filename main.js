7b2720d99ba21d25dff6aa0f7ff689e5143d8041
AyenaCode <ayenacode1@gmail.com>
2024-10-23 03:36:41 -0400

    vaiable

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+import { paragraphText, worldText } from "./module";
 import "./style.css";
+
+{
+  const textEnterByUser = document.getElementById("userText");
+  const validationBtn = document.getElementById("validationBtn");
+
+  const textProposed = document.querySelector(".textSave");
+  const score = document.querySelector(".score");
+
+  const radioBtn = document.querySelectorAll("#world");
+
+  console.log(textEnterByUser, validationBtn, textProposed, score, radioBtn);
+}
+
+console.log(worldText, paragraphText);
+
+const handdleSubmit = (e) => {
+  e.preventDefault();
+  console.log("test");
+};
+
+{
+  validationBtn.addEventListener("click", handdleSubmit);
+}
---
+++ module.js
@@ -0,0 +1,8 @@
+const worldText = ["Cachalot", "Pétunia", "Serviette"];
+const paragraphText = [
+  "Pas de panique !",
+  "La vie, l'univers et le reste",
+  "Merci pour le poisson",
+];
+
+export { paragraphText, worldText };
